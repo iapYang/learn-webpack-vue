f9651d143d43baafb8346c109ac5dd1d18dcdfbb
iapYang <yklorse@gmail.com>
2017-01-03 17:44:33 +0800

    0103

--- dev/components/App.vue
+++ dev/components/App.vue
@@ -1,21 +1,34 @@
 <style lang="scss">
 
-    .app-container {
-        background-color: red;
-    }
+@import "../styles/mixin.scss";
+@import "../styles/reset.scss";
+.app-container {
+    /*background-color: red;*/
+    width: 100%;
+    min-height: 500px;
+}
 
 </style>
 
 <template lang="html">
 
-    <div class="app-container">
-
+<div class="app-container">
+    <div id="tutorial">
+        <Navs></Navs>
     </div>
+</div>
 
 </template>
 
 <script>
+import Navs from './tutorial/Navs.vue';
 
-    console.log('app vue');
+console.log('app vue');
+
+export default {
+    components: {
+        Navs,
+    }
+}
 
 </script>
---
+++ dev/components/tutorial/Navs.vue
@@ -0,0 +1,74 @@
+<style lang="scss" scoped>
+    .navs {
+        position: relative;
+        width: 100%;
+        text-align: center;
+
+        .navs-header {
+            position: relative;
+            display: inline-block;
+
+            li {
+                position: relative;
+                display: inline-block;
+                padding: 18px 30px;
+                background-color: #389dc1;
+                cursor: pointer;
+
+                span {
+                    position: relative;
+                    font-size: 16px;
+                    font-weight: 600;
+                    color: #fff;
+                    text-transform: uppercase;
+                }
+            }
+        }
+    }
+</style>
+
+<template lang="html">
+    <div class="navs">
+        <div class="navs-header">
+            <li v-for="item in items">
+                <span>
+                    {{ item.name }}
+                </span>
+            </li>
+        </div>
+        <div class="navs-body">
+
+        </div>
+    </div>
+</template>
+
+<script>
+
+console.log('nav vue');
+
+export default {
+    data() {
+        return {
+            items: [{
+                name: 'home',
+            },{
+                name: 'project',
+            },{
+                name: 'services',
+            },{
+                name: 'contact',
+            }],
+        }
+    },
+    computed: {},
+    ready() {},
+    attached() {},
+    methods: {
+        clickHandler(){
+
+        }
+    },
+    components: {}
+}
+
+</script>
