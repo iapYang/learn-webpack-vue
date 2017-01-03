95a5aceccfb5954e87ebeed47e665841be511d26
iapYang <yklorse@gmail.com>
2017-01-03 09:47:16 +0800

    0103 update

--- dev/script/index.js
+++ dev/script/index.js
@@ -6,12 +6,14 @@ import router from '../router';
 import store from '../store/index';
 import App from '../component/App.vue';
 
+import Platform from './plugin/platform.js';
+
 
 
 const app = new Vue({
     router,
     store,
     components: {
-        App
+        App,
     },
 }).$mount('#app');
